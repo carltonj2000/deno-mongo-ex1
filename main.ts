698f8810fd3e3f041a152c6b8a38d7af54e0c315
Carlton Joseph <carlton.joseph@gmail.com>
2025-01-11 18:17:43 -0800

    add user, pw and db support

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "npm:mongodb@6.1.0";
 
-async function mongoAccess() {
-  const client = new MongoClient("mongodb://127.0.0.1:27017", {
+async function mongoAccess(connectionString: string) {
+  const client = new MongoClient(connectionString, {
     connectTimeoutMS: 5000,
     serverSelectionTimeoutMS: 5000,
   });
@@ -10,7 +10,20 @@ async function mongoAccess() {
 }
 
 if (import.meta.main) {
-  mongoAccess()
-    .then((e) => console.log("seemed to have worked"))
+  let connectionStr = "mongodb://127.0.0.1:27017";
+  if (Deno.args[0] && Deno.args[0] === "app") {
+    const user = Deno.env.get("MONGO_USER") || null;
+    const password = Deno.env.get("MONGO_PASSWORD") || null;
+    const db = Deno.env.get("MONGO_DB") || null;
+    if (!user || !password || !db) {
+      console.error("User, password or db not found in env file.");
+      Deno.exit(-1);
+    }
+    const encPw = password.replaceAll("#", "%23");
+    console.log({ user, password, encPw });
+    connectionStr = `mongodb://${user}:${encPw}@127.0.0.1:27017/${db}`;
+  }
+  mongoAccess(connectionStr)
+    .then(() => console.log("seemed to have worked"))
     .catch((e) => console.error(e));
 }
