63fba622fdf01e69fc27eadb99bdab997aa95441
Carlton Joseph <carlton.joseph@gmail.com>
2025-01-11 20:20:47 -0800

    add mongoose

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
 import { MongoClient } from "npm:mongodb@6.1.0";
 import { escape } from "node:querystring";
+import mongoose from "npm:mongoose@^6.7";
+
+const getUserPwDb = () => {
+  const user = Deno.env.get("MONGO_USER") || null;
+  const passwordNoEsc = Deno.env.get("MONGO_PASSWORD") || null;
+  const db = Deno.env.get("MONGO_DB") || null;
+  if (!user || !passwordNoEsc || !db) {
+    console.error("User, password or db not found in env file.");
+    Deno.exit(-1);
+  }
+  const password = escape(passwordNoEsc);
+  return { user, password, db };
+};
 
 async function mongoAccess(connectionString: string) {
   const client = new MongoClient(connectionString, {
@@ -10,20 +23,26 @@ async function mongoAccess(connectionString: string) {
   client.close();
 }
 
+async function mongooseAccess(connectionString: string) {
+  const conn = await mongoose.connect(connectionString, {
+    serverSelectionTimeoutMS: 5000,
+  });
+  await conn.disconnect();
+}
+
 if (import.meta.main) {
   let connectionStr = "mongodb://127.0.0.1:27017";
+  const { user, password, db } = getUserPwDb();
   if (Deno.args[0] && Deno.args[0] === "app") {
-    const user = Deno.env.get("MONGO_USER") || null;
-    const password = Deno.env.get("MONGO_PASSWORD") || null;
-    const db = Deno.env.get("MONGO_DB") || null;
-    if (!user || !password || !db) {
-      console.error("User, password or db not found in env file.");
-      Deno.exit(-1);
-    }
-    const pwEsc = escape(password);
-    connectionStr = `mongodb://${user}:${pwEsc}@127.0.0.1:27017/${db}`;
+    connectionStr = `mongodb://${user}:${password}@127.0.0.1:27017/${db}`;
   }
-  mongoAccess(connectionStr)
-    .then(() => console.log("seemed to have worked"))
-    .catch((e) => console.error(e));
+  if (Deno.args[1] && Deno.args[1] === "mongoose") {
+    mongooseAccess(connectionStr)
+      .then(() => console.log("mongoose seemed to have worked"))
+      .catch((e) => console.error(e));
+  } else {
+    mongoAccess(connectionStr)
+      .then(() => console.log("mongo seemed to have worked"))
+      .catch((e) => console.error(e));
+  }
 }
